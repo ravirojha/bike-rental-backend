b4c885a2173faff530080cdb4b090b614be2a4ee
ravi ranjan <ravi.ranjan@bitcs.in>
2022-03-01 23:33:56 +0530

    Port changed

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT;
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
