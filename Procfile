15ad436ee5800b276f7017aaa70b267cf227d5d4
ravi ranjan <ravi.ranjan@bitcs.in>
2022-03-02 13:59:29 +0530

    minor fixes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-Web: npm run start:prod
+web: npm run start:prod
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT;
+  const PORT = String(process.env.PORT);
   app.enableCors();
   await app.listen(PORT || 3000, () => {
     console.log("Application Running in Port " + PORT);
